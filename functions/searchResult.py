bac8d3193da1a1c92d7ef3b6f45e8c6f30a274d3
Nguyen Minh Thuy <ntmthuy.1264140@gmail.com>
2024-01-06 12:41:06 +0700

    Basic search with any keywords in a year

--- functions/searchResult.py
+++ functions/searchResult.py
@@ -114,3 +114,188 @@ def lst_search_result(year_val, keyword):
                                   lst_key_vendor_search, lst_key_item_search) for x in n]
 
     return lst_result
+
+
+def filter_df_byFullKeyword(year_val, keyword):
+    # if year == '2012':
+    # To prevent the warning SettingWithCopyWarning, the solution is to explicitly tell
+    # pandas to make a copy when we create the new dataframe:
+    global df_filter
+
+    if year_val== '2012':
+        df_item = df12.loc[df12['Item Description'] == keyword].copy()
+        df_cat = df12.loc[df12['Category Name'] == keyword].copy()
+        df_vendor = df12.loc[df12['Vendor Name'] == keyword].copy()
+        df_store = df12.loc[df12['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2013':
+        df_item = df13.loc[df13['Item Description'] == keyword].copy()
+        df_cat = df13.loc[df13['Category Name'] == keyword].copy()
+        df_vendor = df13.loc[df13['Vendor Name'] == keyword].copy()
+        df_store = df13.loc[df13['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2014':
+        df_item = df14.loc[df14['Item Description'] == keyword].copy()
+        df_cat = df14.loc[df14['Category Name'] == keyword].copy()
+        df_vendor = df14.loc[df14['Vendor Name'] == keyword].copy()
+        df_store = df14.loc[df14['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2015':
+        df_item = df15.loc[df15['Item Description'] == keyword].copy()
+        df_cat = df15.loc[df15['Category Name'] == keyword].copy()
+        df_vendor = df15.loc[df15['Vendor Name'] == keyword].copy()
+        df_store = df15.loc[df15['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2016':
+        df_item = df16.loc[df16['Item Description'] == keyword].copy()
+        df_cat = df16.loc[df16['Category Name'] == keyword].copy()
+        df_vendor = df16.loc[df16['Vendor Name'] == keyword].copy()
+        df_store = df16.loc[df16['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2017':
+        df_item = df17.loc[df17['Item Description'] == keyword].copy()
+        df_cat = df17.loc[df17['Category Name'] == keyword].copy()
+        df_vendor = df17.loc[df17['Vendor Name'] == keyword].copy()
+        df_store = df17.loc[df17['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2018':
+        df_item = df18.loc[df18['Item Description'] == keyword].copy()
+        df_cat = df18.loc[df18['Category Name'] == keyword].copy()
+        df_vendor = df18.loc[df18['Vendor Name'] == keyword].copy()
+        df_store = df18.loc[df18['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2019':
+        df_item = df19.loc[df19['Item Description'] == keyword].copy()
+        df_cat = df19.loc[df19['Category Name'] == keyword].copy()
+        df_vendor = df19.loc[df19['Vendor Name'] == keyword].copy()
+        df_store = df19.loc[df19['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2020':
+        df_item = df20.loc[df20['Item Description'] == keyword].copy()
+        df_cat = df20.loc[df20['Category Name'] == keyword].copy()
+        df_vendor = df20.loc[df20['Vendor Name'] == keyword].copy()
+        df_store = df20.loc[df20['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2021':
+        df_item = df21.loc[df21['Item Description'] == keyword].copy()
+        df_cat = df21.loc[df21['Category Name'] == keyword].copy()
+        df_vendor = df21.loc[df21['Vendor Name'] == keyword].copy()
+        df_store = df21.loc[df21['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    if year_val == '2022':
+        df_item = df22.loc[df22['Item Description'] == keyword].copy()
+        df_cat = df22.loc[df22['Category Name'] == keyword].copy()
+        df_vendor = df22.loc[df22['Vendor Name'] == keyword].copy()
+        df_store = df22.loc[df22['Store Name'] == keyword].copy()
+
+        if len(df_item) > 0:
+            df_filter = df_item
+        elif len(df_cat) > 0:
+            df_filter = df_cat
+        elif len(df_vendor) > 0:
+            df_filter = df_vendor
+        elif len(df_store) > 0:
+            df_filter = df_store
+
+    return df_filter
+
+
+def filter_overall(df_filter):
+    sales = df_filter['Sale (Dollars)'].sum().round(1)
+    cost = (df_filter['State Bottle Cost'] * df_filter['Bottles Sold']).sum().round(1)
+    grossProfit = (sales - cost).round(1)
+    inv = len(df_filter['Invoice/Item Number'])
+    bottleSold = df_filter['Bottles Sold'].sum()
+    volumeSold = df_filter['Volume Sold (Gallons)'].sum().round(1)
+
+    return sales, cost, grossProfit, inv, bottleSold, volumeSold
--- pages/search.py
+++ pages/search.py
@@ -1,7 +1,8 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc, Output, Input, callback, ALL, ctx, State
+
 from components import cards_search
-from functions import searchResult
+from functions import searchResult, calTotal
 
 tab_style = {
     'borderBottom': '1px solid #d6d6d6',
@@ -15,18 +16,14 @@ tab_selected_style = {
     'color': 'black',
 }
 
-keyword_dict = ['2012', '2013', '2014', '2015']
-
 layout = html.Div([
     html.Img(id="backtotop"),
     dbc.Row([
         dbc.Col(
             dcc.Dropdown(
                 ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
-                placeholder="Select year...",
-                id="dropdown_year_search"
-            ),
-            width=3,
+                placeholder="Select year...", id="dropdown_year_search"
+            ), width=3,
         ),
         dbc.Col(
             dbc.Input(type="text", id="txt_keyword",
@@ -45,46 +42,150 @@ layout = html.Div([
     ], className="m-4"),
 
     dbc.Row([
-        html.H6("Your input:"),
+        # html.H6("Your input:"),
         html.Div(id="lstSearch_group"),
     ], className="m-5"),
 
     dbc.Row([
-        dbc.ListGroup(
-            [
-                dbc.ListGroupItem(
-                    f"Item {i}",
-                    id={"type": "list-keyword-rs", "index": i},
-                    action=True
-                )
-                for i in list(keyword_dict)
-            ],
-            id="list-group",
+        html.H4("Result:", style={"color": "#FF7F50"}),
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("SALES", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_totalSale',
+                                    className="card-title text-center",
+                                    style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_sales_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_sales_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow")
         ),
-    ], className="m-5"),
 
-    dbc.Row([
-        html.H6("Keyword selected:"),
-        html.Div(id="keyword_selected")
-    ], className="m-5"),
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("COST", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_totalCost',
+                                    className="card-title text-center", style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_cost_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_cost_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow")),
 
-    dbc.Row(
-        [
-            html.H4("Result:", style={"color": "#FF7F50"}),
-            dbc.Col(dbc.Card(cards_search.sale, color="info", outline=True, className="shadow")),
-            dbc.Col(dbc.Card(cards_search.cost_sales, color="info", outline=True, className="shadow")),
-            dbc.Col(dbc.Card(cards_search.gross_profit, color="info", outline=True, className="shadow")),
-            dbc.Col(dbc.Card(cards_search.inv, color="info", outline=True, className="shadow")),
-            dbc.Col(dbc.Card(cards_search.bottleSold, color="info", outline=True, className="shadow")),
-            dbc.Col(dbc.Card(cards_search.volumeSold, color="info", outline=True, className="shadow"))
-        ], className="m-4",
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("GROSS PROFIT", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_grossProfit',
+                                    className="card-title text-center", style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_profit_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_profit_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow")),
+
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("NUMBER OF INVOICES", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_inv',
+                                    className="card-title text-center",
+                                    style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_inv_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_inv_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow")),
+
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("BOTTLE SOLD", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_bottleSold',
+                                    className="card-title text-center",
+                                    style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_bottle_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_bottle_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow")),
+
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.H6("VOLUME SOLD", className="text-center")),
+                dbc.CardBody([
+                    dbc.Row(html.H3(id='rs_volumeSold',
+                                    className="card-title text-center", style={"color": "#FF1493"})),
+                    dbc.Row([
+                        dbc.Col([
+                            html.P("Previous", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_volume_previous", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                        dbc.Col([
+                            html.P("Different", className="text-center"),
+                            html.H5("100,000",
+                                    id="card_volume_diff", className="text-center", style={"color": "#1E90FF"})
+                        ]),
+                    ])
+                ]),
+            ], color="info", outline=True, className="shadow"))
+    ], className="m-4",
     ),
 
-    dbc.Row(
-        [
-            dbc.Col(cards_search.month_graph, className="text-center border-end m-3"),
-            dbc.Col(cards_search.dow_graph, className="text-center border-end m-3"),
-        ], className="m-4",
+    dbc.Row([
+        dbc.Col(cards_search.month_graph,
+                className="text-center border-end m-3"),
+        dbc.Col(cards_search.dow_graph,
+                className="text-center border-end m-3"),
+    ], className="m-4",
     ),
 
     html.Div(
@@ -131,20 +232,20 @@ layout = html.Div([
         )
     ),
 
-    dbc.Row(
-        [
-            html.H3("STORE LOCATION", style={"color": "#FF7F50"}),
-            html.Div(id='folium-map-rs')
-        ], className="m-5",
+    dbc.Row([
+        html.H3("STORE LOCATION", style={"color": "#FF7F50"}),
+        html.Div(id='folium-map-rs')
+    ], className="m-5",
     ),
 
     dbc.Button(children=html.Span([html.I(className="fa fa-arrow-up fa-lg")]),
                href="#backtotop", n_clicks=0,
                style=dict(fontSize="35px", backgroundColor="rgb(237, 201, 72)",
-                          textAlign="right", color="rgb(30, 51, 118)", border="none",
-                          marginLeft="90%")),
+                          textAlign="right", color="rgb(30, 51, 118)",
+                          border="none", marginLeft="90%")),
 
 ])
+
 
 @callback(
     Output('lstSearch_group', 'children'),
@@ -154,7 +255,11 @@ layout = html.Div([
 def btnSubmit_click(n_clicks, year_val, key_val):
     global rs_group
 
-    #lst_rs = 'Year: {}. Keyword: {}'.format(year_val, key_val)
+    # lst_rs = 'Year: {}. Keyword: {}'.format(year_val, key_val)
+    # cat FLAVORED
+    # store DUBUQUE
+    # vendor RICARD
+    # item COCONUT
     lst_item = searchResult.lst_search_result(year_val, key_val)
     if len(lst_item) <= 0:
         rs_group = [
@@ -165,23 +270,40 @@ def btnSubmit_click(n_clicks, year_val, key_val):
         rs_group = [
             dbc.Alert("About {} results.".format(len(lst_item)),
                       color="info"),
+            html.Label("{}".format(year_val), id='lb-list-keyword-rs1',
+                       style={"visibility": "hidden"}),
             dbc.ListGroup([
-                    dbc.ListGroupItem(
-                        f"{i}",
-                        id={"type": "list-keyword-rs1", "index": i},
-                        action=True
-                    )
-                    for i in list(lst_item)
-                ], id="list-group1")
+                dbc.ListGroupItem(
+                    f"{i}",
+                    id={"type": "list-keyword-rs1", "index": i},
+                    action=True
+                )
+                for i in list(lst_item)
+            ], id="list-group1")
         ]
     return rs_group
 
+
 @callback(
-    Output("keyword_selected", "children"),
-    Input({'type': 'list-keyword-rs', 'index': ALL}, 'n_clicks'),
+    Output("rs_totalSale", "children"),
+    Output("rs_totalCost", "children"),
+    Output("rs_grossProfit", "children"),
+    Output("rs_inv", "children"),
+    Output("rs_bottleSold", "children"),
+    Output("rs_volumeSold", "children"),
+    Input({'type': 'list-keyword-rs1', 'index': ALL}, 'n_clicks'),
+    Input('lb-list-keyword-rs1', 'children'),
     prevent_initial_call=True
 )
-def searchResult_selected(_):
-    return f"Clicked on Item {ctx.triggered_id.index}"
+def searchResult_selected(_, lb_year):
+    global r_totalSale, r_totalCost, r_grossProfit, r_inv, r_bottleSold, r_volumeSold
 
+    full_key = ctx.triggered_id.index # what is selected from user
+    df_filter = searchResult.filter_df_byFullKeyword(lb_year, full_key)
+    sales, cost, grossProfit, inv, bottleSold, volumeSold = searchResult.filter_overall(df_filter)
 
+    # convert to human format: K, M, B
+    r_totalSale, r_totalCost, r_grossProfit, r_inv, r_bottleSold, r_volumeSold = calTotal.total_general_byYear(sales, cost, grossProfit, inv, bottleSold, volumeSold)
+
+    #lst_rs = 'sales: {}. cost: {}'.format(r_totalSale, r_totalCost)
+    return r_totalSale, r_totalCost, r_grossProfit, r_inv, r_bottleSold, r_volumeSold
